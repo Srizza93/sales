11474e2ff3cf590ce3e30d57a1fcd98432a0162b
Martina Di Marco <martina@ADMs-MacBook-Pro.local>
2022-03-10 23:49:59 +0100

    index.html and app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
+import mount from "./scripts/mount.js";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/index.js";
 import store from "./store/index.js";
 
-const app = createApp(App).use(router).use(store).mount("#app");
+document.body.appendChild(mount());
+
+const app = createApp(App).use(router).use(store).mount(".app-root");
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const HtmlWepbackPluglin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    main: {
+      import: path.resolve(__dirname, "./src/app.js"),
+    },
+  },
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: "vue-loader",
+      },
+      {
+        test: /\.styl(us)?$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "stylus-loader"],
+      },
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.svg$/,
+        use: ["vue-loader", "vue-svg-loader"],
+      },
+      {
+        test: /\.(jpg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWepbackPluglin({
+      title: "Sales",
+      chunks: ["main"],
+    }),
+    new MiniCssExtractPlugin({
+      filename: "main.css",
+    }),
+    new VueLoaderPlugin(),
+  ],
+};
